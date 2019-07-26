7a6625ebd7330a391c7e30d589975028ff0e1ddf
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-26 09:33:16 +0100

    feat: create user model access file with tests

---
+++ users/usersModel.js
@@ -0,0 +1,26 @@
+const db = require('../database/dbConfig.js');
+
+function find() {
+  return db('users').select('id', 'username');
+}
+
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
+
+function add(user) {
+  return db('users')
+    .insert(user, 'id')
+    .then((ids) => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+
+module.exports = {
+  add,
+  find,
+  findById,
+};
---
+++ users/usersModel.spec.js
@@ -0,0 +1,33 @@
+const db = require('../database/dbConfig');
+
+const users = require('./usersModel');
+
+beforeEach(async () => {
+  await db('users').truncate();
+});
+
+describe('users model', () => {
+  describe('add()', () => {
+    it('should add the provided users into the db', async () => {
+      await users.add({ username: 'gaffer', password: '1234' });
+      await users.add({ username: 'yusuf', password: '1234' });
+      const usersDb = await db('users');
+      expect(usersDb).toHaveLength(2);
+    });
+  });
+  describe('find()', () => {
+    it('should get all users from the db', async () => {
+      await users.add({ username: 'gaffer', password: '1234' });
+      const usersFromDb = await users.find();
+      expect(usersFromDb).toHaveLength(1);
+      expect(usersFromDb[0].id).toEqual(1);
+    });
+  });
+  describe('findById()', () => {
+    it('should get a users from the db using the provided id', async () => {
+      await users.add({ username: 'gaffer', password: '1234' });
+      const userFromDb = await users.findById(1);
+      expect(userFromDb.id).toEqual(1);
+    });
+  });
+});
